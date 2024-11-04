7b7b467107425efad7e6a886ef4825b78369b63f
Jon Koops <jonkoops@gmail.com>
2024-11-04 13:28:59 +0100

    Initial project scaffold
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

---
+++ src/main/java/dev/koops/frontend-plugin-test/App.java
@@ -0,0 +1,10 @@
+package dev.koops.frontend-plugin-test;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
